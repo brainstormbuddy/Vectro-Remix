d579d2a562410669d6e5c757fdd106cc27af4d78
Adam Sims <adamsims1203@gmail.com>
2023-11-04 21:06:37 -0700

    Ignore onboarding tests that fail because email verification was removed

--- tests/e2e/onboarding.test.ts
+++ tests/e2e/onboarding.test.ts
@@ -32,7 +32,7 @@ const test = base.extend<{
 	},
 })
 
-test('onboarding with link', async ({ page, getOnboardingData }) => {
+test.skip('onboarding with link', async ({ page, getOnboardingData }) => {
 	const onboardingData = getOnboardingData()
 
 	await page.goto('/')
@@ -102,7 +102,10 @@ test('onboarding with link', async ({ page, getOnboardingData }) => {
 	await expect(page).toHaveURL(`/`)
 })
 
-test('onboarding with a short code', async ({ page, getOnboardingData }) => {
+test.skip('onboarding with a short code', async ({
+	page,
+	getOnboardingData,
+}) => {
 	const onboardingData = getOnboardingData()
 
 	await page.goto('/signup')
